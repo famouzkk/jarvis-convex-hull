1e583081cd67c947831f0e0c9a64c2354488d0c0
famouzkk <famouzkk@gmail.com>
2023-06-07 20:48:19 +0200

    changes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#include <cstdint>
 #include <fstream>
 #include <iostream>
 #include <iterator>
+#include <limits>
 #include <string>
 #include <utility>
 #include <vector>
@@ -20,9 +22,13 @@ const uint32_t WIDTH = 800;
  
 using std::chrono::system_clock;
 std::default_random_engine generator(system_clock::now().time_since_epoch().count());
- 
+
+
+
+
+
+
 struct Point {
- 
     int x, y;
     Point(const int x, const int y) : x(x), y(y) {}
     Point() {
@@ -43,9 +49,13 @@ struct Point {
     friend std::ostream & operator<<(std::ostream &os, const Point& p) {
         return os << "(" << p.x << ", " << p.y << ")";
     }
+
+    // for better usage in raylib
+    const raylib::Vector2 asVec() const {
+        return raylib::Vector2{static_cast<float>(x), static_cast<float>(y)};
+    }
 };
  
-
 float find_angle(const Point& p0, const Point& p1, const Point& p2) {
     float b = std::pow(abs(p1.x - p0.x), 2) + std::pow(abs(p1.y - p0.y), 2);
     float a = std::pow(abs(p1.x - p2.x), 2) + std::pow(abs(p1.y - p2.y), 2);
@@ -61,17 +71,25 @@ float find_angle(const Point& p0, const Point& p1, const Point& p2) {
     return std::acos(angle);
 }
 
+
 class Plane {
     std::vector<Point> points;
+    uint32_t points_amount;
     std::vector<Point> path;
 public:
     Plane(const std::vector<Point>& points) : points(points) {}
-    Plane(int amount_points) {
-        for(int i = 0; i < amount_points; i++) {
+    Plane(uint32_t points_amount) : points_amount(points_amount) {
+        for(uint32_t i = 0; i < points_amount; i++) {
             points.push_back(Point{});
         }
     }
 
+    void randomize_points() {
+        points.clear();
+        for(uint32_t i = 0; i < points_amount; i++) {
+            points.push_back(Point{});
+        }
+    }
 
     Point get_bottommost_point() const {
         auto it = std::max_element(points.begin(), points.end(),  [](const Point& a, const Point& b) {
@@ -87,47 +105,45 @@ public:
         return path;
     }
     void make_path() {
-        std::vector<Point> new_points {points};
-        Point bottommost = get_bottommost_point();
-        path = {Point{0, bottommost.y}, bottommost};
+        path.clear();
+        std::vector<Point> remaining_points {points};
+        path.push_back(get_bottommost_point());
 
-        while(path.front() != path.back()) {
+        // create first fake point as reference for calculating angle 
+        auto second_last = Point{std::numeric_limits<int>::min(), get_bottommost_point().y};
+        auto last = path.back();
 
-            auto second_last = *std::prev(path.end(), 2);
-            auto last = *std::prev(path.end());
-            auto max_angle_element = std::max_element(new_points.begin(), new_points.end(), [&second_last, &last](const Point& p1, const Point& p2) {
+        while(path.front() != path.back() || path.size() == 1) {
+            auto max_angle_element = std::max_element(remaining_points.begin(), remaining_points.end(), [&second_last, &last](const Point& p1, const Point& p2) {
                 float a1 = find_angle(second_last, last, p1);
                 float a2 = find_angle(second_last, last, p2);
-                std::cout << "second_last: " << second_last << std::endl;
-                std::cout << "last: " << last << std::endl;
-                std::cout << "p1: " << p1 << std::endl;
-                std::cout << "a1: " << a1 << std::endl;
-                std::cout << "a2: " << a2 << std::endl;
                 return a1 < a2;
             });
 
             path.push_back(*max_angle_element);
-            new_points.erase(max_angle_element);
-            // remove faked first point
-            if(path.begin()->x == 0) { 
-                path.erase(path.begin());
-            }
-            
+            remaining_points.erase(max_angle_element);
+                      
+            second_last = *std::prev(path.end(), 2);
+            last = *std::prev(path.end());
         }  
     }
+
+    void draw_points() {
+        for(const auto& p: points) {
+            DrawCircleV(p.asVec(), 2.f, raylib::Color::White());
+        }
+    }
+    void draw_path() {
+        for(auto it = path.begin(); it != std::prev(path.end());) {
+            ++it;
+            auto prev = std::prev(it);
+            DrawLineEx(prev->asVec(), it->asVec(), 1.f, raylib::Color::Gray());
+        }
+    }
+
 };
  
-const std::vector<Point> ps {{100, 50}, 
-                            {200, 100},
-                            {200, 200},
-                            {100, 200}};
-//  const std::vector<Point> ps {{111, 111}, 
-//                             {200, 90},
-//                             {31, 200},
-//                             {50, 80},
-//                             {100, 100}, 
-//                             {213,52},
-//                             {113,223}};
+
 std::pair<int, int> split(const std::string& str, char delimiter) {
     std::string token1, token2;
     std::istringstream iss(str);
@@ -157,33 +173,27 @@ int main() {
     
     Plane plane {16};
     plane.make_path();
-    auto points = plane.get_points();
-    auto path = plane.get_path();
     
+
     while(!window.ShouldClose()) {
+        if(IsKeyReleased(KEY_F5)) {
+
+            plane.randomize_points();
+            plane.make_path();
+        }
+
+
         BeginDrawing();
-        raylib::DrawText("Punktów: " + std::to_string(points.size()), 20, 20, 22, raylib::Color::Beige());
-        for(auto& p: points) {
-            DrawCircle(p.x, p.y, 2.f, raylib::Color::White());
-        }
-        for(auto it = path.begin(); it != path.end();) {
-            ++it;
-            if(it != path.end()) {
-                auto prev = std::prev(it);
-                DrawLine(prev->x, prev->y, it->x, it->y, raylib::Color::Gray());
+        {
+            raylib::DrawText("Punktów: " + std::to_string(plane.get_points().size()), 20, 20, 22, raylib::Color::Beige());
+            plane.draw_points();
+            plane.draw_path();
 
-            }
+            window.ClearBackground(raylib::Color::Black());
         }
-
-        window.ClearBackground(raylib::Color::Black());
         EndDrawing();
     }
-    // auto fp = load_from_file("points.txt");
-    // Plane pl{fp};
-    // auto path = pl.get_path();
-    // for(auto& p: path) {
-    //     std::cout << p << std::endl;
-    // }
-    raylib::Vector2 a;
+
+
     return 0;
 }
